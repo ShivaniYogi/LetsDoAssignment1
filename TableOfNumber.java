76d722c8fc2fa17753eab51d6d90f9ac8a107c88
ShivaniYogi <shivaniyogi01@gmail.com>
2021-07-19 12:55:40 +0530

    Second Commit

---
+++ TableOfNumber.java
@@ -0,0 +1,11 @@
+public class TableOfNumber{
+	public static void main(String[] args){
+		int n, i, a;
+		System.out.println("Enter a number :");
+		Scanner sc = new Scanner(System.in);
+		for(i = 1; i<=10; i++){
+			a = n*i;
+			System.out.println("Table of a number is :" +a);
+			}
+	}
+}
