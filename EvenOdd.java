ee0e4abe7e8a823acc1efc6b0bc0ad7f766856e7
ShivaniYogi <shivaniyogi01@gmail.com>
2021-07-19 12:56:31 +0530

    Even odd Commit

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+public class EvenOdd{
+	public static void main(string [] args){
+		int n;
+		System.out.println("Enter a Integer number:");
+		Scanner input = new Scanner(System.in);
+		n = input.nextInt();
+		if(num%2 == 0)
+			System.out.println("Entered number is even");
+		else
+			System.out.println("Entered number is odd")'
+	}
+	}
